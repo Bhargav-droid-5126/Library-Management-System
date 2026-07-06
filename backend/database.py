67182a43b3d2bf0234a9ad57b7deeffccb366c59
Bhargav Ch S <imchsbhargav@gmail.com>
2026-07-06 20:28:41 +0530

    Connect backend to Neon PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,8 @@
 import os
 import psycopg2
 
-conn = psycopg2.connect(
-    host=os.getenv("DB_HOST", "localhost"),
-    database=os.getenv("DB_NAME", "library_db"),
-    user=os.getenv("DB_USER", "postgres"),
-    password=os.getenv("DB_PASSWORD", "sb1234"),
-    port=os.getenv("DB_PORT", "5432")
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+conn = psycopg2.connect(DATABASE_URL)
 
 cursor = conn.cursor()
